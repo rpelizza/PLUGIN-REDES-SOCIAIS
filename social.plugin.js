242fbca518d14ab669fe8fe174a26956fe96d73b
Rafael Pedro Pelizza <rpelizza@users.noreply.github.com>
2017-09-20 09:44:55 -0300

    arquivo css
    add arquivo css

--- social.plugin.js
+++ social.plugin.js
@@ -70,75 +70,7 @@
 						gplus_definido+
 						linkedin_definido+
 						rss_definido+
-					'</ul>').appendTo('body');
-
-					$('ul.socialPlugin').css({
-						'list-style': 'none',
-					    'list-style-type': 'none',
-					    'margin': '0',
-					    'padding': '0',
-					    'background-color': '#fff',
-					    'position': 'fixed',
-					    'top': '50%',
-					    'left': '0',
-					    'z-index': '10',
-					    'transform': 'translate(0,-50%)',
-					    '-webkit-box-shadow': '0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 1px 5px 0 rgba(0, 0, 0, 0.12), 0 3px 1px -2px rgba(0, 0, 0, 0.2)',
-					    'box-shadow': '0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 1px 5px 0 rgba(0, 0, 0, 0.12), 0 3px 1px -2px rgba(0, 0, 0, 0.2)',
-					    'border-radius': '0 3px 3px 0'
-					});
-
-					$('ul.socialPlugin li').css({
-						'margin': '0',
-						'padding': '0'
-					});
-
-					$('ul.socialPlugin li a').css({
-						'margin': '0',
-					    'padding': '0',
-					    'display': 'block',
-					    'padding': '5px 3px'
-					});
-
-					$('ul.socialPlugin li a:hover').css({
-						'text-decoration': 'none',
-						'opacity': '.8'
-					});
-
-					$('ul.socialPlugin li a i').css({
-						'font-size': '30px',
-					    'display': 'block',
-					    'margin': '0 auto',
-					    'text-align': 'center'
-					});
-
-					$('ul.socialPlugin li a i.fa-facebook').css({
-						'color': '#3B5998'
-					});
-
-					$('ul.socialPlugin li a i.fa-youtube').css({
-						'color': '#ff0000'
-					});
-
-					$('ul.socialPlugin li a i.fa-twitter').css({
-						'color': '#1da1f2'
-					});
-
-					$('ul.socialPlugin li a i.fa-instagram').css({
-						'color': '#df3967'
-					});
-
-					$('ul.socialPlugin li a i.fa-google-plus').css({
-						'color': '#d43e33'
-					});
-
-					$('ul.socialPlugin li a i.fa-linkedin').css({
-						'color': '#0077b5'
-					});
-
-					$('ul.socialPlugin li a i.fa-rss').css({
-						'color': '#ff9800'
-					});
+					'</ul>').appendTo('body');					
 				}
 			}			
 
